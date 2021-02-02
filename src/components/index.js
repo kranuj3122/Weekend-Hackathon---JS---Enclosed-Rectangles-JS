5cf5e9c72b27ac308da19b8ab78cce4aa484a2b5
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-02 20:28:26 +0530

    Update index.js
    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,45 @@
 
 function updateStructure(rec1,rec2){
 	//write your code
+	let parent={};
+	let child={};
+	let flag="";
+	if( rec2['top']>=rec1['top'] && rec1['top']+rec1['height']>=rec2['top']+rec2['height'] && 
+	rec2['left']>=rec1['left'] && rec1['left']+rec1['width']>=rec2['left']+rec2['width'] ) {
+		flag="some";
+		parent['top']=rec1['top'];
+		parent['left']=rec1['left'];
+		parent['height']=rec1['height'];
+		parent['width']=rec1['width'];
+		parent['children']=[];
+		child['top']=rec2['top']-rec1['top'];
+		child['left']=rec2['left']-rec1['left'];
+		child['height']=rec2['height'];
+		child['width']=rec2['width'];
+		child['children']=[];
+		parent['children'].push(child);
+	}
+	else if( rec1['top']>rec2['top'] && rec2['top']+rec2['height']>rec1['top']+rec1['height'] && 
+	rec1['left']>rec2['left'] && rec2['left']+rec2['width']>rec1['left']+rec1['width'] ) {
+		flag="some";
+		parent['top']=rec2['top'];
+		parent['left']=rec2['left'];
+		parent['height']=rec2['height'];
+		parent['width']=rec2['width'];
+		parent['children']=[];
+		child['top']=rec1['top']-rec2['top'];
+		child['left']=rec1['left']-rec2['left'];
+		child['height']=rec1['height'];
+		child['width']=rec1['width'];
+		child['children']=[];
+		parent['children'].push(child);
+	}
+	if(flag=="") {
+		return rec1;
+	}
+	else {
+		return parent;
+	}
 }
 
 module.exports = updateStructure;
