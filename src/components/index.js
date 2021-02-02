4bd91e7df5eb80c3eebbb395a97a6c56a5370e1f
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-02 22:51:04 +0530

    Update index.js
    add 'px'

--- src/components/index.js
+++ src/components/index.js
@@ -6,61 +6,64 @@
 //      children: []
 //}
 
-function updateStructure(rec1,rec2){
-	let parent={};
-	let child={};
-	let flag="";
-	if( rec2['top']>rec1['top'] && rec1['top']+rec1['height']>rec2['top']+rec2['height'] && 
-	rec2['left']>rec1['left'] && rec1['left']+rec1['width']>rec2['left']+rec2['width'] ) {
-		flag="some";
-		parent['top']=rec1['top'];
-		parent['left']=rec1['left'];
-		parent['height']=rec1['height'];
-		parent['width']=rec1['width'];
-		parent['children']=[];
-		child['top']=rec2['top']-rec1['top'];
-		child['left']=rec2['left']-rec1['left'];
-		child['height']=rec2['height'];
-		child['width']=rec2['width'];
-		child['children']=[];
+const { Children } = require("react");
+
+function updateStructure(rec1, rec2) {
+	let parent = {};
+	let child = {};
+	let flag = "";
+	if (rec2['top'] > rec1['top'] && rec1['top'] + rec1['height'] > rec2['top'] + rec2['height'] &&
+		rec2['left'] > rec1['left'] && rec1['left'] + rec1['width'] > rec2['left'] + rec2['width']) {
+		flag = "some";
+		parent['top'] = rec1['top'] + 'px';
+		parent['left'] = rec1['left'] + 'px';
+		parent['height'] = rec1['height'] + 'px';
+		parent['width'] = rec1['width'] + 'px';
+		parent['children'] = [];
+		child['top'] = (rec2['top'] - rec1['top']) + 'px';
+		child['left'] = (rec2['left'] - rec1['left']) + 'px';
+		child['height'] = rec2['height'] + 'px';
+		child['width'] = rec2['width'] + 'px';
+		child['children'] = [];
 		parent['children'].push(child);
 	}
-	else if( rec2['top']==rec1['top'] && rec1['top']+rec1['height']==rec2['top']+rec2['height'] && 
-	rec2['left']==rec1['left'] && rec1['left']+rec1['width']==rec2['left']+rec2['width'] ) {
-		flag="some";
-		parent['top']=rec1['top'];
-		parent['left']=rec1['left'];
-		parent['height']=rec1['height'];
-		parent['width']=rec1['width'];
-		parent['children']=[];
-		child['top']=rec2['top']-rec1['top'];
-		child['left']=rec2['left']-rec1['left'];
-		child['height']=rec2['height'];
-		child['width']=rec2['width'];
-		child['children']=[];
+	else if (rec2['top'] == rec1['top'] && rec1['top'] + rec1['height'] == rec2['top'] + rec2['height'] &&
+		rec2['left'] == rec1['left'] && rec1['left'] + rec1['width'] == rec2['left'] + rec2['width']) {
+		flag = "some";
+		parent['top'] = rec1['top'] + 'px';
+		parent['left'] = rec1['left'] + 'px';
+		parent['height'] = rec1['height'] + 'px';
+		parent['width'] = rec1['width'] + 'px';
+		parent['children'] = [];
+		child['top'] = (rec2['top'] - rec1['top']) + 'px';
+		child['left'] = (rec2['left'] - rec1['left']) + 'px';
+		child['height'] = rec2['height'] + 'px';
+		child['width'] = rec2['width'] + 'px';
+		child['children'] = [];
 		parent['children'].push(child);
 	}
-	else if( rec1['top']>rec2['top'] && rec2['top']+rec2['height']>rec1['top']+rec1['height'] && 
-	rec1['left']>rec2['left'] && rec2['left']+rec2['width']>rec1['left']+rec1['width'] ) {
-		flag="some";
-		parent['top']=rec2['top'];
-		parent['left']=rec2['left'];
-		parent['height']=rec2['height'];
-		parent['width']=rec2['width'];
-		parent['children']=[];
-		child['top']=rec1['top']-rec2['top'];
-		child['left']=rec1['left']-rec2['left'];
-		child['height']=rec1['height'];
-		child['width']=rec1['width'];
-		child['children']=[];
+	else if (rec1['top'] > rec2['top'] && rec2['top'] + rec2['height'] > rec1['top'] + rec1['height'] &&
+		rec1['left'] > rec2['left'] && rec2['left'] + rec2['width'] > rec1['left'] + rec1['width']) {
+		flag = "some";
+		parent['top'] = rec2['top'] + 'px';
+		parent['left'] = rec2['left'] + 'px';
+		parent['height'] = rec2['height'] + 'px';
+		parent['width'] = rec2['width'] + 'px';
+		parent['children'] = [];
+		child['top'] = (rec1['top'] - rec2['top']) + 'px';
+		child['left'] = (rec1['left'] - rec2['left']) + 'px';
+		child['height'] = rec1['height'] + 'px';
+		child['width'] = rec1['width'] + 'px';
+		child['children'] = [];
 		parent['children'].push(child);
 	}
-	if(flag=="") {
+	if (flag == "") {
 		return rec1;
 	}
 	else {
 		return parent;
 	}
+	//write your code
 }
 
 module.exports = updateStructure;
