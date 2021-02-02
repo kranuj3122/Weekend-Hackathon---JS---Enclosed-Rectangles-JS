36b2067f6865d328da0c3bc06383a39f5f330fd6
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-02 20:46:29 +0530

    Update index.js
    second commit

--- src/components/index.js
+++ src/components/index.js
@@ -7,12 +7,26 @@
 //}
 
 function updateStructure(rec1,rec2){
-	//write your code
 	let parent={};
 	let child={};
 	let flag="";
-	if( rec2['top']>=rec1['top'] && rec1['top']+rec1['height']>=rec2['top']+rec2['height'] && 
-	rec2['left']>=rec1['left'] && rec1['left']+rec1['width']>=rec2['left']+rec2['width'] ) {
+	if( rec2['top']>rec1['top'] && rec1['top']+rec1['height']>rec2['top']+rec2['height'] && 
+	rec2['left']>rec1['left'] && rec1['left']+rec1['width']>rec2['left']+rec2['width'] ) {
+		flag="some";
+		parent['top']=rec1['top'];
+		parent['left']=rec1['left'];
+		parent['height']=rec1['height'];
+		parent['width']=rec1['width'];
+		parent['children']=[];
+		child['top']=rec2['top']-rec1['top'];
+		child['left']=rec2['left']-rec1['left'];
+		child['height']=rec2['height'];
+		child['width']=rec2['width'];
+		child['children']=[];
+		parent['children'].push(child);
+	}
+	else if( rec2['top']==rec1['top'] && rec1['top']+rec1['height']==rec2['top']+rec2['height'] && 
+	rec2['left']==rec1['left'] && rec1['left']+rec1['width']==rec2['left']+rec2['width'] ) {
 		flag="some";
 		parent['top']=rec1['top'];
 		parent['left']=rec1['left'];
