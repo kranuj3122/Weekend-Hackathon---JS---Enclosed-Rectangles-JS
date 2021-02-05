c26c650d0c5e86b9ce0ecabb915ce157b16fa178
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-05 14:19:18 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -9,51 +9,56 @@
 const { Children } = require("react");
 
 function updateStructure(rec1, rec2) {
+	let top1 = Number(rec1['top'].replace('px', ''));
+	let left1 = Number(rec1['left'].replace('px', ''));
+	let height1 = Number(rec1['height'].replace('px', ''));
+	let width1 = Number(rec1['width'].replace('px', ''));
+	let top2 = Number(rec2['top'].replace('px', ''));
+	let left2 = Number(rec2['left'].replace('px', ''));
+	let height2 = Number(rec2['height'].replace('px', ''));
+	let width2 = Number(rec2['width'].replace('px', ''));
 	let parent = {};
 	let child = {};
 	let flag = "";
-	if (Number(rec2['top'].replace('px', '')) > Number(rec1['top'].replace('px', '')) && Number(rec1['top'].replace('px', '')) + Number(rec1['height'].replace('px', '')) > Number(rec2['top'].replace('px', '')) + Number(rec2['height'].replace('px', '')) &&
-		Number(rec2['left'].replace('px', '')) > Number(rec1['left'].replace('px', '')) && Number(rec1['left'].replace('px', '')) + Number(rec1['width'].replace('px', '')) > Number(rec2['left'].replace('px', '')) + Number(rec2['width'].replace('px', ''))) {
+	if (top2 > top1 && top1 + height1 > top2 + height2 && left2 > left1 && left1 + width1 > left2 + width2) {
 		flag = "some";
-		parent['top'] = rec1['top'];
-		parent['left'] = rec1['left'];
-		parent['height'] = rec1['height'];
-		parent['width'] = rec1['width'];
+		parent['top'] = top1 + 'px';
+		parent['left'] = left1 + 'px';
+		parent['height'] = height1 + 'px';
+		parent['width'] = width1 + 'px';
 		parent['children'] = [];
-		child['top'] = (rec2['top'].replace('px', '') - rec1['top'].replace('px', '')) + 'px';
-		child['left'] = (rec2['left'].replace('px', '') - rec1['left'].replace('px', '')) + 'px';
-		child['height'] = rec2['height'];
-		child['width'] = rec2['width'];
+		child['top'] = (top2 - top1) + 'px';
+		child['left'] = (left2 - left1) + 'px';
+		child['height'] = height2;
+		child['width'] = width2;
 		child['children'] = [];
 		parent['children'].push(child);
 	}
-	else if (Number(rec2['top'].replace('px', '')) == Number(rec1['top'].replace('px', '')) && Number(rec1['top'].replace('px', '')) + Number(rec1['height'].replace('px', '')) == Number(rec2['top'].replace('px', '')) + Number(rec2['height'].replace('px', '')) &&
-		Number(rec2['left'].replace('px', '')) == Number(rec1['left'].replace('px', '')) && Number(rec1['left'].replace('px', '')) + Number(rec1['width'].replace('px', '')) == Number(rec2['left'].replace('px', '')) + Number(rec2['width'].replace('px', ''))) {
+	else if (top1 == top2 && top1 + height1 == top2 + height2 && left1 == left2 && left1 + width1 == left2 + width2) {
 		flag = "some";
-		parent['top'] = rec1['top'];
-		parent['left'] = rec1['left'];
-		parent['height'] = rec1['height'];
-		parent['width'] = rec1['width'];
+		parent['top'] = top1 + 'px';
+		parent['left'] = left1 + 'px';
+		parent['height'] = height1 + 'px';
+		parent['width'] = width1 + 'px';
 		parent['children'] = [];
 		child['top'] = '0px';
 		child['left'] = '0px';
-		child['height'] = rec2['height'];
-		child['width'] = rec2['width'];
+		child['height'] = height2;
+		child['width'] = width2;
 		child['children'] = [];
 		parent['children'].push(child);
 	}
-	else if (Number(rec1['top'].replace('px', '')) > Number(rec2['top'].replace('px', '')) && Number(rec2['top'].replace('px', '')) + Number(rec2['height'].replace('px', '')) > Number(rec1['top'].replace('px', '')) + Number(rec1['height'].replace('px', '')) &&
-		Number(rec1['left'].replace('px', '')) > Number(rec2['left'].replace('px', '')) && Number(rec2['left'].replace('px', '')) + Number(rec2['width'].replace('px', '')) > Number(rec1['left'].replace('px', '')) + Number(rec1['width'].replace('px', ''))) {
+	else if (top1 > top2 && top2 + height2 > top1 + height1 && left1 > left2 && left2 + width2 > left1 + width1) {
 		flag = "some";
-		parent['top'] = rec2['top'];
-		parent['left'] = rec2['left'];
-		parent['height'] = rec2['height'];
-		parent['width'] = rec2['width'];
+		parent['top'] = top2 + 'px';
+		parent['left'] = left2 + 'px';
+		parent['height'] = height2 + 'px';
+		parent['width'] = width2 + 'px';
 		parent['children'] = [];
-		child['top'] = (rec1['top'].replace('px', '') - rec2['top'].replace('px', '')) + 'px';
-		child['left'] = (rec1['left'].replace('px', '') - rec2['left'].replace('px', '')) + 'px';
-		child['height'] = rec1['height'];
-		child['width'] = rec1['width'];
+		child['top'] = (top1 - top2) + 'px';
+		child['left'] = (left1 - left2) + 'px';
+		child['height'] = height1;
+		child['width'] = width1;
 		child['children'] = [];
 		parent['children'].push(child);
 	}
