a971273556f087d4d69f4c20b002fb27c7da930f
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-14 21:53:04 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,50 +7,65 @@
 //}
 
 const { Children } = require("react");
+function contain(a, b) {
+	let ans = {};
+	let child = {};
+	let flag = 0;
+	let t1 = a.top ? parseInt(a.top) : 0;
+	let b1 = a.bottom ? parseInt(a.bottom) : 0;
+	let l1 = a.left ? parseInt(a.left) : 0;
+	let r1 = a.right ? parseInt(a.right) : 0;
+	let h1 = a.height ? parseInt(a.height) : 0;
+	let w1 = a.width ? parseInt(a.width) : 0;
 
-function contain(a,b) {
-	let ans={};
-	let flag=0;
-	let t1=a.top?parseInt(a.top):0;
-	let b1=a.bottom?parseInt(a.bottom):0;
-	let l1=a.left?parseInt(a.left):0;
-	let r1=a.right?parseInt(a.right):0;
-	let h1=a.height?parseInt(a.height):0;
-	let w1=a.width?parseInt(a.width):0;
+	let t2 = b.top ? parseInt(b.top) : 0;
+	let b2 = b.bottom ? parseInt(b.bottom) : 0;
+	let l2 = b.left ? parseInt(b.left) : 0;
+	let r2 = b.right ? parseInt(b.right) : 0;
+	let h2 = b.height ? parseInt(b.height) : 0;
+	let w2 = b.width ? parseInt(b.width) : 0;
 
-	let t2=b.top?parseInt(b.top):0;
-	let b2=b.bottom?parseInt(b.bottom):0;
-	let l2=b.left?parseInt(b.left):0;
-	let r2=b.right?parseInt(b.right):0;
-	let h2=b.height?parseInt(b.height):0;
-	let w2=b.width?parseInt(b.width):0;
-	
-	if(
-		t2>=t1 && l2>=l1 && r2>=r1 && b2>=b1
-	){
-		if(
-			( (h1!==0 &&(t1+h1>=t2+h2 && b1+h1>=b2+h2)) &&
-			(w1!==0 && (l1+w1>=l2+w2 && r1+w1>=r2+w2)) ) || 
-			(h1===0 && width===0)
+	if (
+		t2 >= t1 && l2 >= l1 && r2 >= r1 && b2 >= b1
+	) {
+		if (
+			((h1 !== 0 && (t1 + h1 >= t2 + h2 && b1 + h1 >= b2 + h2)) &&
+				(w1 !== 0 && (l1 + w1 >= l2 + w2 && r1 + w1 >= r2 + w2))) ||
+			(h1 === 0 && width === 0)
 		) {
-			ans={...a, children: [{...b, children: []}]};
-			flag=1;
+			if (t1 != 0) {
+				child.top = `${t2 - t1}px`;
+			}
+			if (l1 != 0) {
+				child.left = `${l2 - l1}px`;
+			}
+			if (b1 != 0) {
+				child.bottom = `${b2 - b1}px`;
+			}
+			if (r1 != 0) {
+				child.right = `${r2 - r1}`;
+			}
+			if (h1 != 0) {
+				child.height = `${h2}px`;
+				child.width = `${w2}px`;
+			}
+			ans = { ...a, children: [{ ...child, children: [] }] };
+			flag = 1;
 		}
 
 	}
-	if(flag===0) {
+	if (flag === 0) {
 		return -1;
 	}
 	return ans;
 }
 function updateStructure(rec1, rec2) {
-	let res=contain(rec1,rec2);
-	if(res!==-1)
-	{
+	let res = contain(rec1, rec2);
+	if (res !== -1) {
 		return res;
 	}
-	res=contains(rec2,rec1);
-	if(res!==-1) {
+	res = contain(rec2, rec1);
+	if (res !== -1) {
 		return res;
 	}
 	return rec1;
